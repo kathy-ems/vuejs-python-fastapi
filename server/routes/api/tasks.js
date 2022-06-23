544eaab5acc965fe1ac862f2f3d8b7b414185296
Kathy Ems <kathy-ems@users.noreply.github.com>
2022-06-23 13:38:53 -0700

    deletes a task

--- server/routes/api/tasks.js
+++ server/routes/api/tasks.js
@@ -6,10 +6,21 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const tasksCollection = await loadTasksCollection();
-    // const numOfTasks = tasks.collection.count();
     res.send(await tasksCollection.find({}).sort({ order: -1 }).toArray());
   } catch (e) {
     console.log("Unable to get tasks", e);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const tasksCollection = await loadTasksCollection();
+    await tasksCollection.deleteOne({
+      _id: new mongodb.ObjectId(req.params.id),
+    });
+    res.status(200).send();
+  } catch (e) {
+    console.log("Unable to delete task", e);
   }
 });
 
