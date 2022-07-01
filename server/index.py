5575dc16eb5ba38be296ac55d9e30492282d1085
Kathy Ems <kathy-ems@users.noreply.github.com>
2022-07-01 16:59:18 -0700

    adds task get and post routes

--- server/index.py
+++ server/index.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Data": "Test"} # python dictionary
+class Task(BaseModel):
+  text: str
+  completed: bool
+  order: int
+
+
+tasks = {
+  1: {
+    "text": "Milk",
+    "completed": True
+  }
+}
+
+@app.get("/tasks/")
+def get_task(_id: int = Path(None, description="The ID of the task")):
+    return tasks
+
+@app.get("/tasks/{_id}")
+def get_task(_id: int = Path(None, description="The ID of the task")):
+    return tasks[_id]
+
+@app.post("/tasks/")
+def add_task(task: Task):
+  return tasks #empty dictionary
